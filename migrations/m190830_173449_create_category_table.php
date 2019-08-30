fa032fa339c8c473de69cd6ad1f43c3f36174ae1
Marat <yeffasol@mail.ru>
2019-08-30 20:48:02 +0300

    Add migrations

---
+++ migrations/m190830_173449_create_category_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%category}}`.
+ */
+class m190830_173449_create_category_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function up()
+    {
+        $this->createTable('category', [
+            'id' => $this->primaryKey(),
+            'title'=>$this->string()
+        ]);
+    }
+    /**
+     * @inheritdoc
+     */
+    public function down()
+    {
+        $this->dropTable('category');
+    }
+}
---
+++ migrations/m190830_173507_create_tag_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%tag}}`.
+ */
+class m190830_173507_create_tag_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function up()
+    {
+        $this->createTable('tag', [
+            'id' => $this->primaryKey(),
+            'title'=>$this->string()
+        ]);
+    }
+    /**
+     * @inheritdoc
+     */
+    public function down()
+    {
+        $this->dropTable('tag');
+    }
+}
